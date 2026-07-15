a994e04750c2b1d5468449f2503f2ea970b7ccc1
niwatorichan <charloslivro2@gmail.com>
2026-07-15 17:05:20 -0400

    Better handling for Savage with gamemode

--- hosts/savage/default.nix
+++ hosts/savage/default.nix
@@ -14,5 +14,27 @@
   # Hostname
   networking.hostName = "Savage";
 
+  # Add "Return to Gaming Mode" shortcut on Desktop
+  home-manager.users.niwatorichan = { pkgs, ... }: {
+    home.file."Desktop/Return-to-Gaming-Mode.desktop" = {
+      text = ''
+        [Desktop Entry]
+        Name=Return to Gaming Mode
+        Exec=steamos-session-select gamescope
+        Icon=steam
+        Terminal=false
+        Type=Application
+        StartupNotify=false
+      '';
+      executable = true;
+    };
+  };
 
+  # Speed up session transitions (gaming mode <-> desktop mode) by reducing timeouts
+  systemd.extraConfig = ''
+    DefaultTimeoutStopSec=10s
+  '';
+  systemd.user.extraConfig = ''
+    DefaultTimeoutStopSec=10s
+  '';
 }
