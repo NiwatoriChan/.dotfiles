e0cf0f8e2ddc445c362c0d1f0b5af546a03cad2e
niwatorichan <charloslivro2@gmail.com>
2026-08-06 08:29:13 -0400

    Trying Steam Deck related stuff

--- hosts/savage/default.nix
+++ hosts/savage/default.nix
@@ -20,7 +20,7 @@
       text = ''
         [Desktop Entry]
         Name=Return to Gaming Mode
-        Exec=/run/current-system/sw/bin/steamos-session-select gamescope
+        Exec=steamos-session-select gamescope
         Icon=steam
         Terminal=false
         Type=Application
