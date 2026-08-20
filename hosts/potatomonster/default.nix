16ec7925d42f64a0670376d3fc6e7877730d9592
niwatorichan <charloslivro2@gmail.com>
2026-08-20 15:27:28 -0400

    Add syncthing to PotatoMonster

--- hosts/potatomonster/default.nix
+++ hosts/potatomonster/default.nix
@@ -17,7 +17,7 @@
     ../../modules/virtualisation/vmware.nix
     ../../modules/developpement.nix
     ../../modules/multimedias.nix
-    #../../modules/syncthing
+    ../../modules/syncthing
   ];
 
   # Hostname
